891742f5b4a98132d805316ef9b53a183e04ad63
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-11-04 12:00:30 -0200

    search database uri and base_url via environment variables

--- devnone/conf.py
+++ devnone/conf.py
@@ -1,5 +1,10 @@
+import os
+
+
 DEBUG = False
 REDIS_DB = 1
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite://')
+BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000/')
 
 try:
     from devnone.conf_local import *
