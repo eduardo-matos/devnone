8c34d56c53cc7f96e49906ee83ae711f0e567983
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-11-25 19:08:40 -0200

    configure logger

--- devnone/__init__.py
+++ devnone/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import RotatingFileHandler
 import os
 from flask import Flask
 
@@ -8,3 +10,10 @@ if os.environ.get('prod'):
     app.config.from_object('devnone.conf')
 else:
     app.config.from_object('devnone.conf_dev')
+
+# logging
+handler = RotatingFileHandler(os.path.join(os.path.dirname(__file__), 'logs', 'devnone.log'), backupCount=10)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter('{"%(levelname)s":"%(asctime)s", '
+                                       '"%(funcName)s":%(lineno)d, "%(threadName)s":"%(message)s"}'))
+app.logger.addHandler(handler)
